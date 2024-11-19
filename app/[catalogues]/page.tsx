e55ace634d05b6535a0e8814996fe7d570aaf600
Suraj Kr Saw <surajkartik71@gmail.com>
2024-11-19 23:36:47 +0530

    Ignoring Eslint rules during build

--- app/[catalogues]/page.tsx
+++ app/[catalogues]/page.tsx
@@ -14,36 +14,53 @@ const Catalogues = ({ params }: { params: { catalogues: string } }) => {
         <div className="flex flex-wrap -m-4">
           {products
             .filter(
-              (product: any) =>
-                product.category === decodeURIComponent(params.catalogues)
+              (product: {
+                id: number;
+                name: string;
+                code: string;
+                price: string;
+                category: string;
+                image: string;
+              }) => product.category === decodeURIComponent(params.catalogues)
             )
-            .map((product: any) => (
-              <div key={product.id} className="lg:w-1/3 md:w-1/2 p-5">
-                <Link href="#">
-                  <div className="p-2 bg-gray-100 hover:bg-white rounded-lg shadow-sm hover:shadow-2xl transition-shadow duration-200">
-                    <Image
-                      height={3000}
-                      width={3000}
-                      alt={product.category + " " + product.name}
-                      className="object-cover object-center w-full h-full rounded-md"
-                      src={product.image}
-                    />
-                    <div className="mt-4">
-                      <h3 className="text-xs font-thin tracking-widest text-gray-400">
-                        {decodeURIComponent(params.catalogues).toUpperCase()}
-                      </h3>
-                      <h2 className="text-2xl font-bold tracking-widest text-blue-900 text-right overline">
-                        {product.name}
-                      </h2>
-                      <div className="flex justify-between text-gray-800">
-                        <p className="mt-1">Code {product.code}</p>
-                        <p className="mt-1 text-green-600">₹ {product.price}</p>
+            .map(
+              (product: {
+                id: number;
+                name: string;
+                code: string;
+                price: string;
+                category: string;
+                image: string;
+              }) => (
+                <div key={product.id} className="lg:w-1/3 md:w-1/2 p-5">
+                  <Link href="#">
+                    <div className="p-2 bg-gray-100 hover:bg-white rounded-lg shadow-sm hover:shadow-2xl transition-shadow duration-200">
+                      <Image
+                        height={3000}
+                        width={3000}
+                        alt={product.category + " " + product.name}
+                        className="object-cover object-center w-full h-full rounded-md"
+                        src={product.image}
+                      />
+                      <div className="mt-4">
+                        <h3 className="text-xs font-thin tracking-widest text-gray-400">
+                          {decodeURIComponent(params.catalogues).toUpperCase()}
+                        </h3>
+                        <h2 className="text-2xl font-bold tracking-widest text-blue-900 text-right overline">
+                          {product.name}
+                        </h2>
+                        <div className="flex justify-between text-gray-800">
+                          <p className="mt-1">Code {product.code}</p>
+                          <p className="mt-1 text-green-600">
+                            ₹ {product.price}
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
+                  </Link>
+                </div>
+              )
+            )}
         </div>
       </div>
     </section>
